bb00ee182671e858e42d3cc7472417f63c5b1c6d
Ashley Thompson <workashleythompson@gmail.com>
2023-09-01 21:02:39 +1000

    adds sqlite3 & visitor logic

--- index-ws.js
+++ index-ws.js
@@ -2,19 +2,27 @@ const express = require('express')
 const server = require('http').createServer()
 const app = express()
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.sendFile('index.html', { root: __dirname })
 })
 
 server.on('request', app)
 server.listen(3000, () => console.log('Listening on port 3000'))
 
+// SIGNAL INTERRUPT... DO, SOMETHING...
+process.on('SIGINT', () => {
+  wss.clients.forEach(function each(client) {
+    client.close()
+  })
+  server.close(() => {
+    shutdownDB()
+  })
+})
 /** Begin Web Socket */
 const WebSocketServer = require('ws').Server
 
 const wss = new WebSocketServer({ server })
 
-// function connectino(ws)
 wss.on('connection', function connection(ws) {
   const numClients = wss.clients.size
   console.log('Clients connected: ', { numClients })
@@ -25,6 +33,11 @@ wss.on('connection', function connection(ws) {
   // an enum of connection states
   if (ws.readyState === ws.OPEN) {
     ws.send('Welcome to my server')
+
+    db.run(`
+      INSERT INTO visitors (count, time)
+      VALUES (${numClients}, datetime('now'))
+    `)
   }
 
   ws.on('close', function close() {
@@ -37,4 +50,34 @@ wss.broadcast = function broadcast(data) {
   wss.clients.forEach(function each(client) {
     client.send(data)
   })
+}
+
+/** End Web Socket */
+/** Begin Database */
+const sqlite = require('sqlite3')
+// can also use fsfe.db (.db is a convention, not requirement)
+const db = new sqlite.Database(':memory:')
+
+// Setup the DB before it runs
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+    )
+  `)
+})
+
+// Typically we want to use "PREPARED STATEMENTS", not just free-hand SQL...
+// BIG seurity concern!
+function getCounts() {
+  db.each('SELECT * FROM visitors', (_err, row) => {
+    console.log(row)
+  })
+}
+
+function shutdownDB() {
+  getCounts()
+  console.log('Shutting down 👋')
+  db.close()
 }
